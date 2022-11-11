a6e60fed34396abec1635a42b849068c984bef66
Stephen Feest <steve84@gmail.com>
2022-11-11 14:03:10 +0000

    Add question submission form
    Again, due to the time constraints on this exercise this is a very basic
    implementation.
    
    In a real application I would redirect unauthenticated users to log in
    first so as to prevent questions from being submitted anonymously.

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -18,4 +18,57 @@ RSpec.describe 'Questions' do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'questions#new' do
+    it 'renders the registration form' do
+      get new_question_path
+
+      expect(response.body).to include 'Ask a question'
+    end
+
+    it "responds with a 'Success' HTTP response status" do
+      get new_question_path
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'question#create' do
+    let(:title) { 'Why is the sky blue?' }
+    let(:question_params) { { title:, body: 'Is it because the clouds contain water?' } }
+    let(:question) { Question.find_by(title:) }
+    let(:post_create) { post questions_path, params: { question: question_params } }
+
+    context 'when the user is logged in' do
+      include_context 'with a logged in user'
+
+      it 'sets the current user as the author' do
+        post_create
+
+        expect(question.author).to eq current_user
+      end
+
+      it 'redirects to the question' do
+        post_create
+
+        expect(response).to redirect_to question
+      end
+
+      context 'when the form contains invalid attributes' do
+        let(:title) { 'a' * 500 }
+
+        it 're-renders the registration form with the error summary' do
+          post_create
+
+          expect(response.body).to include 'Title is too long'
+        end
+
+        it "has an 'Unprocessable Entity' HTTP response status" do
+          post_create
+
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+  end
 end
